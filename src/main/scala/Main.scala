2e1b5d83ac3d691e087a1befb34f408fd24b2466
FreeFly19 <oleksandr.melnyk19@gmail.com>
2018-02-14 15:32:33 +0200

    Add roulette number ordering by frequency

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,21 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object Main {
+  private val spark: SparkSession = SparkSession.builder()
+    .master("local")
+    .appName("hwm-bot")
+    .getOrCreate()
+
+  import spark.implicits._
+
+  def main(args: Array[String]): Unit = {
+    val dataFile = spark.read
+      .option("header", "true")
+      .csv("/Users/freefly/temp/hwm/data.csv")
+
+    dataFile.groupByKey(_.getString(3)).count().orderBy($"count(1)").show(40)
+
+
+  }
+}
